afc59e875cde96317f0cff6fa623253c53e52078
thales <thalespvalente2@proton.me>
2024-10-24 16:29:57 -0300

    primeiro botão

--- login.py
+++ login.py
@@ -1,11 +1,16 @@
+from tkinter import Button
+
 from customtkinter import *
+
+def clique_aqui():
+    print("button_1")
 
 janela = CTk()
 janela.geometry("400x400")
 janela.resizable(False,False)
 janela._set_appearance_mode("dark")
 
-
+CTkButton(janela, command=clique_aqui, text="clique aqui").pack()
 
 
 janela.mainloop()
